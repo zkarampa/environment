f7f1626f89c05984d0e2eae0e63323714cf59ee4
Zisis Karampaglis <zisis.karampaglis@constant.co>
2016-04-03 00:10:07 +0100

    #1 Adds a setup script

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+prefix="=====>"
+path=`pwd`
+dotfiles_dir="${path}/dotfiles"
+
+echo "${prefix} Removing old stuff..."
+
+file="bashrc"
+
+if [[ -L ~/.${file} ]]; then
+    rm ~/.${file}
+elif [[ -f ~/.${file} ]]; then
+    mv ~/.${file} ~/.${file}.bak
+fi
+
+echo "${prefix} Creating symlinks..."
+if [[ ! -f ~/.${file} ]]; then
+    ln -s ${dotfiles_dir}/${file} ~/.${file} > /dev/null
+fi
+
+echo "${prefix} Reload your shell for changes to take place..."
+
+exit 0
