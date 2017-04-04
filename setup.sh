be8c217786c366c39d1d921e51bba2f268b06137
Zisis K <zkarampa@gmail.com>
2017-04-04 21:53:40 +0100

    Add vimrc and delete moc confs

--- setup.sh
+++ setup.sh
@@ -4,25 +4,24 @@ prefix="=====>"
 path=`pwd`
 dotfiles_dir="${path}/dotfiles"
 
+declare -a files=("bashrc" "vimrc" "gitconfig" "gitignore_global")
+
 echo "${prefix} Removing old stuff..."
 
-declare -a files=("bashrc" "gitconfig" "gitignore_global")
-
 for file in ${files[@]}; do
-    if [[ -L ~/.${file} ]]; then
+    if [[ -L ~/.${file} ]]; then # True if FILE exists and is a symbolic link.
         rm ~/.${file}
-    elif [[ -f ~/.${file} ]]; then
+    elif [[ -f ~/.${file} ]]; then # True if FILE exists and is a regular file.
         mv ~/.${file} ~/.${file}.bak
     fi
 done
 
 echo "${prefix} Creating symlinks..."
 for file in ${files[@]}; do
-    if [[ ! -f ~/.${file} ]]; then
+    if [[ ! -f ~/.${file} ]]; then  # True if NOT(FILE exists and is a regular file).
         ln -s ${dotfiles_dir}/${file} ~/.${file} > /dev/null
     fi
 done
-
 
 echo "${prefix} Reload your shell for changes to take place..."
 
